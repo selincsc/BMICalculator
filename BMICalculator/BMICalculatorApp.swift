f998ce3c5595eb4ed902ee7d9f757a622852d985
Halil Ibrahim Oztekin <35626045+halilibrahimoztekin@users.noreply.github.com>
2022-03-26 21:28:13 +0300

    Initial Commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by Halil İbrahim Öztekin on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMICalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BMICalculator
+//
+//  Created by Halil İbrahim Öztekin on 26.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
